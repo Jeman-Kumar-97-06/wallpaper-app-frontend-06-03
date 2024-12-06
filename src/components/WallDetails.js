8958d4b223d89521e6c7fc7c55eb58cf237bf048
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-12-06 21:19:31 +0530

    Made Changes

--- src/components/WallDetails.js
+++ src/components/WallDetails.js
@@ -26,7 +26,7 @@ const WallDetails = ({wall}) => {
     }
     return (
         <div className='wall_details'>
-            <img src={wall.wall} id='each_wall_img' alt="wallpaper_missing :("/>
+            <img src={"https://wallpaper-app-backend.onrender.com/"+wall.wall} id='each_wall_img' alt="wallpaper_missing :("/>
             <button id='download_button' onClick={handleDownload}><span className="material-icons">download</span></button>
         </div>
     )
